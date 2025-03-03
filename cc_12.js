da680839d86301304c787f769b3248f177f5dee0
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-03-02 22:41:25 -0500

    Task 1 - Created Revenue Metric Card.

---
+++ cc_12.js
@@ -0,0 +1,23 @@
+// Task 1: Business Dashboard- DOM element selection 
+const dashboardById = document.getElementById("dashboard");    // selecting by ID 
+const dashboardByQuery = document.querySelector("#dashboard"); // selecting using the querySelector 
+   // creating  a metric card
+function createMetricCard(id, title) {
+    const card = document.createElement("div");
+    card.setAttribute("class", "metric-card");
+    card.setAttribute("id", id);
+    card.innerHTML = `
+        <h3>${title}</h3>
+        <p>$0</p>
+    `;
+    return card;
+}
+
+// Added 3 metric cards and the basic starting values 
+dashboardById.appendChild(createMetricCard("revenueCard", "Revenue"));
+dashboardById.appendChild(createMetricCard("profitCard", "Profit"));
+dashboardById.appendChild(createMetricCard("expensesCard", "Expenses"));
+// value: 
+document.getElementById("revenueCard").querySelector("p").innerText = "$160";
+document.getElementById("profitCard").querySelector("p").innerText = "$100";
+document.getElementById("expensesCard").querySelector("p").innerText = "$90";
